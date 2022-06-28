ee1296a2d79752371c5bc2d79b9c493a69397242
Mike <mgoldobin@psu.ru>
2022-06-28 14:31:51 +0500

    add main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom'
+import PublicPage from './pages/PublicPage'
+import PrivatePage from './pages/PrivatePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PublicPage />} />
+        <Route path="/private" element={<PrivatePage />} />
+        <Route path="/*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
